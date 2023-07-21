aee9eabccfa45719fef8badcc961ef45670a062a
Houston Haynes <h3techsme@gmail.com>
2023-07-21 18:33:56 -0400

    Calculate Y angle and echo to console

---
+++ src/SunFlwrMeadow/Helpers.fs
@@ -0,0 +1,15 @@
+﻿module Helpers
+
+open Meadow.Units
+
+// Standard factor to calculate a degree value from the "raw" 
+// meters-per-second-squared output from the accelerometer
+let radToDeg = 57.29578
+
+// Subtracting 180.0 here is to "flip" the value such that when 
+// the device is facing up the reported Y angle value is "0".
+// This allows a direct association with positioning the panel
+// relative to +/- 22 degrees from solar noon
+let calculateAccYangle (x: Acceleration) (z: Acceleration) =
+    ((atan2 x.MetersPerSecondSquared z.MetersPerSecondSquared + System.Math.PI) * radToDeg) - 180.0
+
--- src/SunFlwrMeadow/Program.fs
+++ src/SunFlwrMeadow/Program.fs
@@ -1,53 +1,44 @@
 ﻿namespace SunFlwrMeadow
 
 open System
+open System.Threading.Tasks
 open Meadow
 open Meadow.Devices
-open Meadow.Foundation.Sensors.Motion
-open System.Threading.Tasks
 open Meadow.Hardware
+open Meadow.Foundation.Sensors.Motion
 open Meadow.Units
+open Helpers
 
 type MeadowApp() =
-    // Change F7FeatherV2 to F7FeatherV1 for V1.x boards
     inherit App<F7FeatherV2>()
 
     let mutable Accelerometer : Adxl345 = null
     let mutable accelerometerData : Acceleration3D = Acceleration3D()
 
+    let mutable boardYoffset = 0.0
+    let mutable boardYangle = 0.0
+
     override this.Initialize() =
         Resolver.Log.Info("Initialize...")
-        
-        let i2cBus = MeadowApp.Device.CreateI2cBus(I2cBusSpeed.Standard)
 
-        // Create a new ADXL345 sensor on the I2C bus
+        let i2cBus = MeadowApp.Device.CreateI2cBus(I2cBusSpeed.Standard)
         Accelerometer <- new Adxl345(i2cBus)
         Accelerometer.SetPowerState(false, false, true, false, Adxl345.Frequencies.FourHz)
-
-        // classical .NET events can also be used:
-        Accelerometer.Updated.Add(fun args ->
-            accelerometerData <- args.New
-            Resolver.Log.Info(
-                sprintf "Accel: [X:%f, Y:%f, Z:%f (m/s^2)]" 
-                        accelerometerData.X.MetersPerSecondSquared 
-                        accelerometerData.Y.MetersPerSecondSquared 
-                        accelerometerData.Z.MetersPerSecondSquared
-            )
-        )
-
+        let offsetResult = Async.AwaitTask (Accelerometer.Read())
+        accelerometerData <- Async.RunSynchronously offsetResult
+        boardYoffset <- calculateAccYangle accelerometerData.X accelerometerData.Z
+        Resolver.Log.Info(sprintf "Y offset: %.1f%%" boardYoffset)
+        
         Task.CompletedTask
 
     override this.Run() =
         async {
-            let updatingTask = async { Accelerometer.StartUpdating(TimeSpan.FromMilliseconds(250.0)) }
-            
-            // Use do! to handle the result of Async.StartImmediate
-            do! updatingTask
-
-            Resolver.Log.Info(
-                sprintf "Final Accel: [X:%f, Y:%f, Z:%f (m/s^2)]" 
-                        accelerometerData.X.MetersPerSecondSquared 
-                        accelerometerData.Y.MetersPerSecondSquared 
-                        accelerometerData.Z.MetersPerSecondSquared
-            )
+            do! async { Accelerometer.StartUpdating(TimeSpan.FromMilliseconds(250.0)) }
+    
+            while true do
+                let! result = Async.AwaitTask (Accelerometer.Read())
+                accelerometerData <- result
+                boardYangle <- calculateAccYangle accelerometerData.X accelerometerData.Z - boardYoffset
+                Resolver.Log.Info(sprintf "Y angle: %.1f%%" boardYangle)
+                do! Async.Sleep(TimeSpan.FromMilliseconds(250.0))
         } |> Async.StartAsTask :> Task
