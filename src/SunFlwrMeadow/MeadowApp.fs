2aee0a64485af88f9f4c765248f8d70cab2b83c3
Houston Haynes <h3techsme@gmail.com>
2023-07-24 11:05:32 -0400

    add accelerometer echo to console

--- src/SunFlwrMeadow/MeadowApp.fs
+++ src/SunFlwrMeadow/MeadowApp.fs
@@ -1,15 +1,25 @@
 ﻿namespace SunFlwrMeadow
 
+open System
 open System.Threading
 open System.Threading.Tasks
 open Meadow.Foundation.Leds
 open Meadow
 open Meadow.Devices
+open Meadow.Hardware
 open Meadow.Gateways.Bluetooth
 open SunFlwrMeadow.LedController
+open Meadow.Foundation.Sensors.Motion
+open Meadow.Units
+open Helpers
 
 type MeadowApp() =
     inherit App<F7FeatherV2>()
+
+    let mutable Accelerometer : Adxl345 = null
+    let mutable accelerometerData : Acceleration3D = Acceleration3D()
+    let mutable boardYoffset = 0.0
+    let mutable boardYangle = 0.0
     
     let rgbLed =
         RgbLed(MeadowApp.Device.Pins.OnboardLedRed, MeadowApp.Device.Pins.OnboardLedGreen, MeadowApp.Device.Pins.OnboardLedBlue)
@@ -58,7 +68,28 @@ type MeadowApp() =
         let mutable getRandomeColor = Unchecked.defaultof<ICharacteristic>
         Resolver.Log.Info("Initialize...")
         bleTreeDefinition <- Definition("MeadowRGB", this.GetDefinition())
-        MeadowApp.Device.BluetoothAdapter.StartBluetoothServer(bleTreeDefinition)
+        MeadowApp.Device.BluetoothAdapter.StartBluetoothServer(bleTreeDefinition) |> ignore
+
+        Resolver.Log.Info("Initialize Accelerometer...")
+        
+        let i2cBus = MeadowApp.Device.CreateI2cBus(I2cBusSpeed.Standard)
+        Accelerometer <- new Adxl345(i2cBus)
+        Accelerometer.SetPowerState(false, false, true, false, Adxl345.Frequencies.FourHz)
+        
+        // find the initial fractional degree position of the board to
+        // compensate relative to new changes when the panel is repositioned
+        let offsetResult = Async.AwaitTask (Accelerometer.Read())
+        accelerometerData <- Async.RunSynchronously offsetResult
+        boardYoffset <- calculateAccYangle accelerometerData.X accelerometerData.Z
+                
+        match boardYoffset with 
+            | x when x > -2.0 && x < 2.0 ->
+                // set "offset" if board is between +/- 2 degrees of level
+                boardYoffset <- x
+                Resolver.Log.Info(sprintf "Y offset: %.1f%%" boardYoffset)
+            | _ -> 
+                // otherwise exit
+                failwith $"Intiial angle out of range. [{boardYoffset}]" 
 
         base.Initialize()
 
@@ -81,4 +112,15 @@ type MeadowApp() =
             ledController.StartRunningColors()
 
             do! Async.Sleep(Timeout.Infinite) // Keep the app running indefinitely
+        } |> Async.StartAsTask :> Task |> ignore
+
+        async {
+            do! async { Accelerometer.StartUpdating(TimeSpan.FromMilliseconds(250.0)) }
+           
+            while true do
+                let! result = Async.AwaitTask (Accelerometer.Read())
+                accelerometerData <- result
+                boardYangle <- calculateAccYangle accelerometerData.X accelerometerData.Z - boardYoffset
+                Resolver.Log.Info(sprintf "Y angle: %.1f%%" boardYangle)
+                do! Async.Sleep(TimeSpan.FromMilliseconds(250.0))
         } |> Async.StartAsTask :> Task
